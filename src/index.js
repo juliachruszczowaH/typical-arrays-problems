0161a2542d628a1dc871c394ab05d1b790451851
juliachruszczowaH <julia.chruszczowa@gmail.com>
2021-07-13 00:35:54 +0300

    add solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
+exports.min = function min(array) {
+    return !array || array.length === 0 ? 0 : array.sort((a, b) => a - b)[0];
+};
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.max = function max(array) {
+    return !array || array.length === 0 ? 0 : array.sort((a, b) => b - a)[0];
+};
 
-exports.max = function max (array) {
-  return 0;
-}
-
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.avg = function avg(array) {
+    return !array || array.length === 0
+        ? 0
+        : array.reduce((sum, current) => (sum = sum + current)) / array.length;
+};
